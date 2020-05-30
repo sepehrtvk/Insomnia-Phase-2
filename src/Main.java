3e5e574b210bd32c1b5340fa33cf5c63c8e90b48
9831111 <9831111@ceit.local>
2020-05-30 21:55:41 +0430

    Add post method.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
@@ -11,6 +9,11 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        try {
+            sendPOST("http://apapi.haditabatabaei.ir/tests/post/formdata");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         while (true) {
             String input = scanner.nextLine();
             String[] words = input.split(" ");
@@ -25,6 +28,7 @@ public class Main {
                     e.printStackTrace();
                 }
             }
+
 
         }
 
@@ -56,40 +60,39 @@ public class Main {
         }
 
     }
-//    private static void sendPOST(String url) throws IOException {
-//        URL obj = new URL(url);
-//        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-//        con.setRequestMethod("POST");
-//        con.setRequestProperty("User-Agent", "Mac_Catalina");
-//
-//        // For POST only - START
-//        con.setDoOutput(true);
-//        OutputStream os = con.getOutputStream();
-//        os.write(Integer.parseInt("90"));
-//        os.flush();
-//        os.close();
-//        // For POST only - END
-//
-//        int responseCode = con.getResponseCode();
-//        System.out.println("POST Response Code :: " + responseCode);
-//
-//        if (responseCode == HttpURLConnection.HTTP_OK) { //success
-//            BufferedReader in = new BufferedReader(new InputStreamReader(
-//                    con.getInputStream()));
-//            String inputLine;
-//            StringBuffer response = new StringBuffer();
-//
-//            while ((inputLine = in.readLine()) != null) {
-//                response.append(inputLine);
-//            }
-//            in.close();
-//
-//            // print result
-//            System.out.println(response.toString());
-//        } else {
-//            System.out.println("POST request not worked");
-//        }
-//    }
+    private static void sendPOST(String url) throws IOException {
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
+        // Setting basic post request
+        con.setRequestMethod("POST");
+        con.setRequestProperty("User-Agent", "Mac_Catalina");
+        con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+        con.setRequestProperty("Content-Type","application/json");
+
+        String postJsonData = "{}";
+
+        // Send post request
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(postJsonData);
+        wr.flush();
+        wr.close();
+
+        int responseCode = con.getResponseCode();
+        System.out.println("nSending 'POST' request to URL : " + url);
+        System.out.println("Post Data : " + postJsonData);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String output;
+        while ((output = in.readLine()) != null) {
+            System.out.println(output);
+        }
+        in.close();
+
+        //printing result from response
+    }
 
 }
+
