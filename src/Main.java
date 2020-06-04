9236bf074ab38e884e87c19a1e02bb5047e58b54
9831111 <9831111@ceit.local>
2020-06-05 00:55:25 +0430

    Get fire and list commands from args.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
@@ -10,8 +12,45 @@ public class Main {
             String input = scanner.nextLine();
             args = input.split("\\s+");
         }
+        if (args[0].toLowerCase().equals("-list") || args[0].toLowerCase().equals("-fire")) {
 
+            if (args[0].toLowerCase().equals("-list")) {
 
+                try {
+
+                    Scanner sc = new Scanner(new File("commands.sav"));
+                    int i = 1;
+                    while (sc.hasNext()) {
+
+                        System.out.println(i + ": " + sc.nextLine());
+                        i++;
+                        sc.nextLine();
+                    }
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (args[0].toLowerCase().equals("-fire")) {
+
+                int n = Integer.parseInt(args[1]);
+
+                try {
+
+                    Scanner sc = new Scanner(new File("commands.sav"));
+                    for (int i = 1; i < n; i++) {
+
+                        sc.nextLine();
+                        sc.nextLine();
+                    }
+                    sc.nextLine();
+                    Request request = new Request(sc.nextLine().split("\\s+"));
+                    request.send();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
     }
 }
